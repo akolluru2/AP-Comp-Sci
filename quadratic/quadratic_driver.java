f91f568ad6d654fd4a7a568f48f00374732449e0
akolluru2 <alexrider990@gmail.com>
2016-09-27 21:29:59 -0700

    Create quadratic_driver.java

---
+++ quadratic/quadratic_driver.java
@@ -0,0 +1,6 @@
+public class quadratic_driver(){
+  public static void main(String[]args){
+    quadratic_equation quad1=new quadratic_equation(3,-9,-12);
+    System.out.println(quad1.toString());
+  }
+}
