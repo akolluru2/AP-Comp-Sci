f345d3e7f8872d8da1e48d68906afb9efa971e6e
akolluru2 <alexrider990@gmail.com>
2016-10-19 09:47:32 -0700

    Update door.java

--- chap3labs/door.java
+++ chap3labs/door.java
@@ -1,3 +1,20 @@
 public class Door{
-  public Door(String name, )
+  private boolean isOpen = false;
+  private String name = "";
+  public Door(String name, boolean open){
+    this.name = name;
+    isOpen = open;
+  }
+  public void close(){
+    isOpen = false; 
+  }
+  public void open(){
+    isOpen = true; 
+  }
+  public void setName(String name){
+    this.name = name; 
+  }
+  public String getName(){
+    return name; 
+  }
 }
