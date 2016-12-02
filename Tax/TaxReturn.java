b077847c5a79d35b5ebaeb98b4e3cac7502e2dd9
akolluru2 <alexrider990@gmail.com>
2016-12-02 09:21:15 -0800

    Update TaxReturn.java

--- Tax/TaxReturn.java
+++ Tax/TaxReturn.java
@@ -1,23 +1,22 @@
 /**
    A tax return of a taxpayer in 2008.
 */
-public class TaxReturn
-{  
-   	public static final int SINGLE = 1;
-   	public static final int MARRIED = 2;
+public class TaxReturn{  
+   public static final int SINGLE = 1;
+   public static final int MARRIED = 2;
 
-private static final double RATE1 = 0.15;
-private static final double RATE2 = 0.28;
-private static final double RATE3 = 0.31;
+   private static final double RATE1 = 0.15;
+   private static final double RATE2 = 0.28;
+   private static final double RATE3 = 0.31;
 
-private static final double SINGLE_BRACKET1 = 21450;
-private static final double SINGLE_BRACKET2 = 51900;
+   private static final double SINGLE_BRACKET1 = 21450;
+   private static final double SINGLE_BRACKET2 = 51900;
 
-private static final double MARRIED_BRACKET1 = 35800;
-private static final double MARRIED_BRACKET2 = 86500;
+   private static final double MARRIED_BRACKET1 = 35800;
+   private static final double MARRIED_BRACKET2 = 86500;
 
-   	private double income;
-   	private int status;
+   private double income;
+   private int status;
 
    /**
       Constructs a TaxReturn object for a given income and 
