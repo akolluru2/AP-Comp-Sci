cee8bff6db3a4e277721001568cafbf02ee3d855
akolluru2 <alexrider990@gmail.com>
2016-12-06 09:08:08 -0800

    Create type_conversion.java

---
+++ type_conversion.java
@@ -0,0 +1,25 @@
+if(val == 1){
+  System.out.println("______");
+}else if(val == 10){
+  System.out.println("_______");
+}else{
+  System.out.println("__________");
+}
+
+String greeting = "hello";
+if(greeting.equals "hello"){
+  // do something for true like return 1 or true
+}else{
+  // do something for false like return 0 or false
+}
+
+count = 1;
+
+int order = 500; // can be any number
+if(order >= 100){ //number 100 depends on when the discount starts
+  double discount = (order / 10)/100;
+  double final = order - (order * discount);
+}
+
+int = 1
+String = 5.0hello122.0
