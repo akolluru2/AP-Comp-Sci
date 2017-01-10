eb365e145a68f75b46a03c7948ed7e1422822ccc
akolluru2 <alexrider990@gmail.com>
2017-01-10 08:57:04 -0800

    Create PartB.java

---
+++ Arrays/PartB.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class names {
+
+	public static void main(String[] args) {
+		String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
+		String [] mName = {"F.", "G.", "H.", "I."};
+		String [] lName = {"Jackman", "Kincaid", "Lacy"};
+		ArrayList<String> allName = new ArrayList<String>();
+		for(int i = 0; i < fName.length; i++){
+			for(int j = 0; j < fName.length; j++){
+				for(int k = 0; k < fName.length; k++){
+					allName.add(fName[i]+" "+mName[j]+" "+lName[i]);
+				}
+			}
+		}
+		System.out.println(allName);
+
+	}
+
+}
