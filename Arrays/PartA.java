f6a5f94227e4a4cb1f0ba5d7a281eae80bfbaf08
akolluru2 <alexrider990@gmail.com>
2017-01-10 09:15:53 -0800

    Update PartA.java

--- Arrays/PartA.java
+++ Arrays/PartA.java
@@ -4,5 +4,20 @@ public class names {
 		String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
 		String [] mName = {"F.", "G.", "H.", "I."};
 		String [] lName = {"Jackman", "Kincaid", "Lacy"};
-  }
+		String [] names = new String [10];
+		names[0] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[1] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[2] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[3] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[4] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[5] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[6] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[7] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[8] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[9] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		for(int i = 0; i < 10; i++){
+			String name = names[i];
+			System.out.println(name);
+		}
+	}
 }
