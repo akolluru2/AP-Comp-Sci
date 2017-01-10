711bd0a2890ea68595fbe185a8e966f4f3594d3a
akolluru2 <alexrider990@gmail.com>
2017-01-10 08:51:34 -0800

    Create PartA.java

---
+++ Arrays/PartA.java
@@ -0,0 +1,8 @@
+public class names {
+
+	public static void main(String[] args) {
+		String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
+		String [] mName = {"F.", "G.", "H.", "I."};
+		String [] lName = {"Jackman", "Kincaid", "Lacy"};
+  }
+}
