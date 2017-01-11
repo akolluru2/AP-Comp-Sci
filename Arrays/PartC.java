4eafd80d040459f49fdb1ccd1a454b147176b8d9
akolluru2 <alexrider990@gmail.com>
2017-01-11 10:14:50 -0800

    Update PartC.java

--- Arrays/PartC.java
+++ Arrays/PartC.java
@@ -4,13 +4,13 @@ pubic class Array{
 		String [] mName = {"F.", "G.", "H.", "I."};
 		String [] lName = {"Jackman", "Kincaid", "Lacy"};
 		String [] names = new String [10];
-	  for(int l = 0; l<names.length; l++){
-      names[l] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+	  for(int i = 0; i<names.length; i++){
+      names[i] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
     }
-    for(int i = 0; i< names.length; i++){
-			for(int j = i; j<names.length; j++){
-				if(names[i].equals(names[j])){
-					names[j] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];	
+    for(int j = 0; j< names.length; j++){
+			for(int k = j; k<names.length; k++){
+				if(names[j].equals(names[k])){
+					names[k] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];	
 				}
 			}
 		}
