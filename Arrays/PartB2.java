e948aa834072baf0a1687fa1e6b9acb8b0d5104b
akolluru2 <alexrider990@gmail.com>
2017-01-11 09:42:55 -0800

    Update PartB2.java

--- Arrays/PartB2.java
+++ Arrays/PartB2.java
@@ -3,7 +3,7 @@ pubic class arrays{
     String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
 		String [] mName = {"F.", "G.", "H.", "I."};
 		String [] lName = {"Jackman", "Kincaid", "Lacy"};
-    for(int i = 0; i < fName.length-1; i+2){
+    for(int i = 0; i < fName.length-1; i + 2){
       String test = fName[i];
       String test2 = fName[i+1];
       fName[i] = test2;
