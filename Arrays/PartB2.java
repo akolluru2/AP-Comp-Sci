68e211db2bbaccb167367e5f155afecfc4fa1024
akolluru2 <alexrider990@gmail.com>
2017-01-11 09:41:14 -0800

    Update PartB2.java

--- Arrays/PartB2.java
+++ Arrays/PartB2.java
@@ -1,5 +1,13 @@
 pubic class arrays{
   public static void main(String[]args){
-    git
+    String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
+		String [] mName = {"F.", "G.", "H.", "I."};
+		String [] lName = {"Jackman", "Kincaid", "Lacy"};
+    for(int i = 0; i < fName.length-1; i+2){
+      String test = fName[i];
+      String test2 = fName[i+1];
+      fName[i] = test2;
+      fName[i+1] = test;
+    }
   }
 }  
