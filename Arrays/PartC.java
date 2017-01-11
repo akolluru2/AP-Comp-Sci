2d07a738e463eb5b52ef33ba3c6f632833af3bd7
akolluru2 <alexrider990@gmail.com>
2017-01-11 09:57:13 -0800

    Create PartC.java

---
+++ Arrays/PartC.java
@@ -0,0 +1,19 @@
+pubic class Array{
+  public static void main(String[]args){
+		String [] fName = {"Andy", "Barbara", "Chandra", "Dyson", "Eric"};
+		String [] mName = {"F.", "G.", "H.", "I."};
+		String [] lName = {"Jackman", "Kincaid", "Lacy"};
+		String [] names = new String [10];
+		names[0] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[1] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[2] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[3] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[4] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[5] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[6] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[7] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[8] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+		names[9] = fName[(int)(Math.random()*5)]+" "+mName[(int)(Math.random()*4)]+" "+lName[(int)(Math.random()*3)];
+    
+  }
+}
