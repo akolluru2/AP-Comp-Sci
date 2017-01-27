4462371f7832d177f1468a20ef6422176bb03f3f
akolluru2 <alexrider990@gmail.com>
2017-01-26 20:37:52 -0800

    Update 4.java

--- Chap7Labs/4.java
+++ Chap7Labs/4.java
@@ -1,1 +1,10 @@
-
+publlic class 2D{
+  public static void main(String[] args){
+    int [] [] nums = new int [4] [5];
+    for(int i = 0; i < 4; i++){
+      for(int j = 0; j < 5; j++){
+        nums [i] [j] = (int) ((Math.random()*48)+2);
+      }
+    }
+  }
+}
