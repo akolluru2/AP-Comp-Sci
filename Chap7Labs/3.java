28354baaed13cd365c96563c606cedb892988de6
akolluru2 <alexrider990@gmail.com>
2017-01-26 20:04:19 -0800

    Update 3.java

--- Chap7Labs/3.java
+++ Chap7Labs/3.java
@@ -1,2 +1,9 @@
 public class ForEach{
-  public static 
+  public static void main(String[] args){
+    //assume values is an array of doubles
+    double sum;
+    for(double nums : values){
+      sum = sum + nums
+    }
+  }
+}
