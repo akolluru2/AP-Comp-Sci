7554f52e1fc165efdc43538ebd28b97a415486e9
akolluru2 <alexrider990@gmail.com>
2017-01-27 09:04:55 -0800

    Update 7.java

--- Chap7Labs/7.java
+++ Chap7Labs/7.java
@@ -1,3 +1,10 @@
+import java.util.Arrays;
 public class sort{
-
+  public static void main (String[] args){
+    int[] rands = new int[20];
+    for(int i = 0; i < rands.length; i++){
+      rands[i] = (int)((Math.random()*100));
+    }
+    Arrays.sort(rands);
+  }
 }
