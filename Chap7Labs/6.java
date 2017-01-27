1b64fdf355149860fd4b1493483bf147beecf98e
akolluru2 <alexrider990@gmail.com>
2017-01-27 08:54:34 -0800

    Update 6.java

--- Chap7Labs/6.java
+++ Chap7Labs/6.java
@@ -10,4 +10,25 @@ public class ArrayMethods{
     values[length-1] = first;
     values[0] = last;
   }
+  public void shiftRight(){
+    int [] reorg = new int[values.length];
+    for(int i = 0; i < values.length; i++){
+      if(i = values.length-1){
+        reorg[0] = values[i];
+      }else{
+        reorg[i] = values[i+1];
+      }
+    }
+    for(int j = 0; j < values.length; j==){
+      values[i] = reorg[i];
+    }
+  }
+  public void replaceEven(){
+    for(int i = 0; i <values.length; i++){
+      if(values[i]%2 = 0){
+        values[i] = 0;
+      }
+    }
+  }
+  
 }
