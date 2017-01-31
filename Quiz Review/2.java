3210da704490a709654e2e92bd3b4a7ba8166e6e
akolluru2 <alexrider990@gmail.com>
2017-01-31 08:45:10 -0800

    Create 2.java

---
+++ Quiz Review/2.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class RandomStringChooser{
+private ArrayList<String> words = new ArrayList<String>();
+  publlic RandomStringChooser(ArrayList words){
+    this.words = words;
+  }
+}
