404ce05527ca30b19a6f1947c69f1163441ccb12
akolluru2 <alexrider990@gmail.com>
2017-01-31 08:58:20 -0800

    Update 2.java

--- Quiz Review/2.java
+++ Quiz Review/2.java
@@ -4,4 +4,14 @@ private ArrayList<String> words = new ArrayList<String>();
   publlic RandomStringChooser(ArrayList words){
     this.words = words;
   }
+  public static String getNext()
+    String word;
+    if(words.size == 0){
+      word = "NONE";
+    }else{
+      int random = (int)((Math.random()*words.size());
+      word = words.get(random);
+      words.remove(random);
+    }
+    return word;
 }
